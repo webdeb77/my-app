51f7dfde7d3e83f9d27ccf2dd6863940bbe9505b
webdeb77 <122753316+webdeb77@users.noreply.github.com>
2023-02-02 22:41:32 +0300

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import './Navbar.module.css';
+import classes from './Navbar.module.css';
 
 const Navbar = () => {
     return (
-        <nav className='nav'>
-            <div className='item'>
+        <nav className={classes.nav}>
+            <div className={classes.item}>
                 <a>Profile</a>
             </div>
-            <div className='item'>
+            <div className={classes.item}>
                 <a>Messeges</a>
             </div>
-            <div className='item'>
+            <div className={classes.item}>
                 <a>News</a>
             </div>
-            <div className='item'>
-            <a>Musick</a>
+            <div className={classes.item}>
+                <a>Musick</a>
             </div>
-            <div className='item'>
+            <div className={classes.item}>
                 <a>Settingss</a>
             </div>
         </nav>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import './Profile.css';
+import s from './Profile.module.css';
 
 const Profile = () => {
-    return  <div className='content'>
+    return  <div className={s.content}>
     <div>
-    <img src='https://phonoteka.org/uploads/posts/2021-06/1624306054_56-phonoteka_org-p-oboi-plyazh-krasivo-65.jpg'/>
+       <img src='https://phonoteka.org/uploads/posts/2021-06/1624306054_56-phonoteka_org-p-oboi-plyazh-krasivo-65.jpg'/>
     </div>
     <div>
-      ava
+      ava + description
     </div>
     <div>
       my posts
       <div>
-        new posts
-      </div>
-      <div>
+        new post
+    </div>
+    <div className={s.posts}>
+      <div className={s.item}>
         post 1
       </div>
-      <div>
+      <div className='item'>
         post 2 
+      </div>
       </div>
     </div>
   </div>
